12c896fc0668d6abab50535ef76d5dcc9def6fd2
Tsvetelina Borisova <tsvetelina@emerchantpay.com>
2014-07-29 16:52:27 +0300

    Change client.py and server.py
    Now players can write messages that are seen by other players
    only when 4 players are connected and when it is the player's turn
    to write

--- client.py
+++ client.py
@@ -1,63 +1,57 @@
-#1. forbid all player to write
-#2. when player throws and moves all players have to see
-#client sends to clientchannel which sends to server which sends to client
 import sys
 from time import sleep
 from sys import stdin, exit
-from player import *
 
 from PodSixNet.Connection import connection, ConnectionListener
 
+# This example uses Python threads to manage async input from sys.stdin.
+# This is so that I can receive input from the console whilst running the server.
+# Don't ever do this - it's slow and ugly. (I'm doing it for simplicity's sake)
 from threading import *
 
-class Client(ConnectionListener, Player): #client sends messages to clientchannel which is in chatserver
+class Client(ConnectionListener):
     def __init__(self, host, port):
         self.Connect((host, port))
         print("Chat client started")
         print("Ctrl-C to exit")
-        print("Choose color: ")
+        # get a nickname from the user before starting
+        print("Enter your nickname: ")
         connection.Send({"action": "nickname", "nickname": stdin.readline().rstrip("\n")})
+        # launch our threaded input loop
         t = Thread(target=self.InputLoop).start()
-        connection.Send({"action": "message", "message": 'pa'})
-
-
+    
     def Loop(self):
         connection.Pump()
         self.Pump()
-
+    
     def InputLoop(self):
         # horrid threaded input loop
         # continually reads from stdin and sends whatever is typed to the server
         while 1:
-            #should be check if player can write
-            # if connection.Send({'action': 'player_can_write', 'player_color': self.color()}):
             connection.Send({"action": "message", "message": stdin.readline().rstrip("\n")})
-
+    
     #######################################
     ### Network event/message callbacks ###
     #######################################
-
+    
     def Network_players(self, data):
         print("*** players: {0}".format(", ".join([p for p in data['players']])))
-
+    
     def Network_message(self, data):
-        print(data['message'])
-
+        print(data['who'] + ": " + data['message'])
+    
     # built in stuff
 
     def Network_connected(self, data):
         print("You are now connected to the server")
-
+    
     def Network_error(self, data):
-        print('error:'.format(data['error'][1]))
+        print("error: {0}".format(data['error'][1]))
         connection.Close()
-
+    
     def Network_disconnected(self, data):
         print('Server disconnected')
         exit()
-
-    def players_colors(self):
-        return ['R', 'B', 'G', 'Y'].pop()
 
 if len(sys.argv) != 2:
     print("Usage: {0} host:port".format(sys.argv[0]))
--- server.py
+++ server.py
@@ -1,25 +1,9 @@
 import sys
 from time import sleep, localtime
 from weakref import WeakKeyDictionary
-from cli import CLI
-from player import Player
 
 from PodSixNet.Server import Server
 from PodSixNet.Channel import Channel
-
-BOARD = """
-                  38 39 00
-                  37 40 01
-            r     36 41 02      b
-                  35 42 03
-      30 31 32 33 34 43 04 05 06 07 08
-      29 52 53 54 55 56 47 46 45 44 09
-      28 27 26 25 24 51 14 13 12 11 10
-                  23 50 15
-                  22 49 16
-           y      21 48 17    g
-                  20 19 18
-"""
 
 class ClientChannel(Channel):
     """
@@ -28,66 +12,68 @@ class ClientChannel(Channel):
     def __init__(self, *args, **kwargs):
         self.nickname = "anonymous"
         Channel.__init__(self, *args, **kwargs)
-
+    
     def Close(self):
         self._server.DelPlayer(self)
-
+    
     ##################################
     ### Network specific callbacks ###
     ##################################
-
+    
     def Network_message(self, data):
-        print('client send {0}'.format(data))
-        self._server.SendToAll({"action": "message", "message": BOARD})
-
-    def Network_player_can_write(self, data):
-        if self._server.game_is_started():
-            return self._server.player_can_write(data['player_color']) == self._server.current_player
-        else:
-            return False
-
+        if self._server.player_can_write(self):
+            self._server.change_player()
+            self._server.SendToAll({"action": "message", "message": data['message'], "who": self.nickname})
+    
     def Network_nickname(self, data):
         self.nickname = data['nickname']
         self._server.SendPlayers()
 
-class ChatServer(Server, CLI): #CLI cli sends messages to all
+class ChatServer(Server):
     channelClass = ClientChannel
-
+    
     def __init__(self, *args, **kwargs):
         Server.__init__(self, *args, **kwargs)
+        self.current_player = 0
         self.players = WeakKeyDictionary()
         print('Server launched')
-
+    
     def Connected(self, channel, addr):
         self.AddPlayer(channel)
-
+    
     def AddPlayer(self, player):
-        print("New Player {0}".format(str(player.addr)))
-        self.players[player] = True
-        self.SendPlayers()
-        if len(self.players) == 2:
-            print("will start")
-            self.cli = CLI(RedPlayer([RedPlayer(), BluePlayer()]))
-        print("players {0}".format([p for p in self.players]))
-
+        if len(self.players) < 4:
+            print("New Player {0}".format(str(player.addr)))
+            self.players[player] = True
+            self.SendPlayers()
+            if len(self.players) == 4:
+                self.current_player = 0
+            print("players {0}".format([p for p in self.players]))
+    
     def DelPlayer(self, player):
         print("Deleting Player {0}".format(str(player.addr)))
         del self.players[player]
         self.SendPlayers()
-
+    
     def SendPlayers(self):
         self.SendToAll({"action": "players", "players": [p.nickname for p in self.players]})
-
-    def SendToAll(self, data): #sends data to all players
+    
+    def SendToAll(self, data):
         [p.Send(data) for p in self.players]
-
-    def game_is_started(self):
-        hasattr(self, 'cli')
-
+    
     def Launch(self):
         while True:
             self.Pump()
             sleep(0.0001)
+
+    def player_can_write(self, player):
+        return len(self.players) == 4 and self.current_player == list(self.players.keys()).index(player)
+
+    def change_player(self):
+        if self.current_player == 3:
+            self.current_player = 0
+        else:
+            self.current_player += 1
 
 # get command line argument of server, port
 if len(sys.argv) != 2:
